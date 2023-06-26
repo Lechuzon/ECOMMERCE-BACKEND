8909c3e5fdc7bf359ee6ab9d166b8d8bf97493ab
Karla Teves <52722830+irene3000@users.noreply.github.com>
2023-06-25 20:22:31 -0500

    avance

--- controllers/blogCtrl.js
+++ controllers/blogCtrl.js
@@ -15,6 +15,7 @@ const createBlog = asyncHandler(async (req,res) =>{
 
 const updateBlog = asyncHandler(async (req,res) =>{
     const { id } = req.params;
+    validateMongoDbId(id);
     try{
         const updateBlog = await Blog.findByIdAndUpdate(id, req.body ,{
             new:true,
@@ -28,20 +29,88 @@ const updateBlog = asyncHandler(async (req,res) =>{
 
 const getBlog = asyncHandler(async (req,res) =>{
     const { id } = req.params;
+    validateMongoDbId(id);
     try{
         const getBlog = await Blog.findById(id);
-        await Blog.findByIdAndUpdate(
+        const updateViews = await Blog.findByIdAndUpdate(
             id,
             {
-                $inc:{numViews:1},
+                $inc: { numViews: 1 },
             },
-            { new:true }
+            { new: true }
         );
-        res.json(getBlog);
+        res.json(updateViews);
     }
     catch (error){
         throw new Error(error);
     }
 });
 
-module.exports = { createBlog, updateBlog, getBlog };
+const getAllBlogs = asyncHandler(async (req, res) => {
+    try{
+        const getBlogs = await Blog.find();
+        res.json(getBlogs);
+    }
+    catch (error){
+        throw new Error(error);
+    }
+});
+
+const deleteBlog = asyncHandler(async (req, res) =>{
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try{
+        const deletedBlog = await Blog.findByIdAndDelete(id);
+        res.json(deletedBlog);
+    }
+    catch (error){
+        throw new Error(error);
+    }
+});
+
+const liketheBlog = asyncHandler(async (req, res) =>{
+    const { blogId } = req.body;
+    validateMongoDbId(blogId);
+
+    // Find the blog  which you want to be liked
+    const blog = await Blog.findById(blogId);
+    // find the login user 
+    const loginUserId = req?.user?._id;
+    //find if the user has liked the blog
+    const isLiked = blog?.isLiked;
+    //find if the user has disliked the blog
+    const alreadyDisliked = blog?.dislikes?.find(
+        (userId) => userId?.toString() === loginUserId?.toString()
+    );
+    if (alreadyDisliked) {
+        const blog = await Blog.findByIdAndUpdate(blogId,{
+            $pull: { dislikes: loginUserId },
+            isDisliked:false,
+        },
+        { new: true }
+        );
+        res.json(blog);
+    }
+    if (isLiked){
+        const blog = await Blog.findByIdAndUpdate(blogId,{
+            $pull: { likes: loginUserId },
+            isLiked: false,
+        },
+        { new: true }
+        );
+        res.json(blog);
+    }
+    else {
+        const blog = await Blog.findByIdAndUpdate(blogId,{
+            $push: { likes: loginUserId },
+            isLiked: true,
+        },
+        { new: true }
+        );
+        res.json(blog);
+    }
+});
+
+
+
+module.exports = { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, liketheBlog, };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { createBlog, updateBlog } = require("../controllers/blogCtrl");
+const { createBlog, updateBlog, getBlog, getAllBlogs,deleteBlog, liketheBlog } = require("../controllers/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
 router.post('/',authMiddleware, isAdmin, createBlog);
+router.put('/likes', authMiddleware, liketheBlog);
+
 router.put('/:id',authMiddleware, isAdmin, updateBlog);
-router.get('/:id', updateBlog);
+
+router.get('/:id', getBlog);
+router.get('/', getAllBlogs);
+
+router.delete('/:id', authMiddleware, isAdmin, deleteBlog);
 
 
 module.exports = router;
