e94ca1e60ec189ac67e2af85cb8baf1e273ff900
Karla Teves <52722830+irene3000@users.noreply.github.com>
2023-08-05 09:53:47 -0500

    añadiendo cors

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const couponRouter = require("./routes/couponRoute");
 
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-dbConnect();
+const cors = require("cors");
 
+dbConnect();
 app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
