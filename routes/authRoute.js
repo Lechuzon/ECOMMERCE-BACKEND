cb4613232a30cd2b2032b65fab5159fb63492d91
Karla Teves <52722830+irene3000@users.noreply.github.com>
2023-08-06 14:40:55 -0500

    111

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -23,6 +23,7 @@ const {
   createOrder,
   getOrders,
   updateOrderStatus,
+  getAllOrders,
 } = require("../controllers/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
